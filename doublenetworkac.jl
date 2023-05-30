59f1a946108382a5f066c6bafd8e8fc9395fba7c
Karim Naguib <karimn2.0@gmail.com>
2023-05-30 11:29:26 -0400

    Separate double models from algo

--- doublenetworkac.jl
+++ doublenetworkac.jl
@@ -1,20 +1,23 @@
 struct DoubleNetworkActorCriticModel{PM, VM} <: AbstractActorCriticModel where {PM <: AbstractPolicyModel, VM <: AbstractValueModel}
     policymodel::PM
     valuemodel::VM
-    policymodelopt
-    valuemodelopt
+    opt
 end
+
+@functor DoubleNetworkActorCriticModel (policymodel, valuemodel)
 
 function DoubleNetworkActorCriticModel{PM, VM}(ninputdims::Int, noutputdims::Int, policyhiddendims::Vector{Int}, valuehiddendims::Vector{Int}, policyopt::Flux.Optimise.AbstractOptimiser, valueopt::Flux.Optimise.AbstractOptimiser;
                                                usegpu = true) where {PM <: AbstractPolicyModel, VM <: AbstractValueModel}
     policymodel = PM(ninputdims, noutputdims, policyhiddendims; usegpu)
     valuemodel = VM(ninputdims, valuehiddendims; usegpu)
 
-    DoubleNetworkActorCriticModel{PM, VM}(policymodel, valuemodel, Flux.setup(policyopt, policymodel), Flux.setup(valueopt, valuemodel))
+    DoubleNetworkActorCriticModel{PM, VM}(policymodel, valuemodel, (policymodel = Flux.setup(policyopt, policymodel), valuemodel = Flux.setup(valueopt, valuemodel)))
 end
 
 𝒱(m::DoubleNetworkActorCriticModel, state) = 𝒱(m.valuemodel, state)
 π(m::DoubleNetworkActorCriticModel, state) = π(m.policymodel, state)
+𝒬(m::DoubleNetworkActorCriticModel, state, action) = 𝒬(m.valuemodel, state, action)
+ℒᵥ(m::DoubleNetworkActorCriticModel, v̂, v) = ℒ(m.valuemodel, v̂, v) 
 
 selectaction(m::DoubleNetworkActorCriticModel, state; rng::AbstractRNG = Random.GLOBAL_RNG, usegpu = true) = selectaction(m.policymodel, state; rng, usegpu)
 selectgreedyaction(m::DoubleNetworkActorCriticModel, state; usegpu = true) = selectgreedyaction(m.policymodel, state; usegpu)
@@ -23,16 +26,11 @@ function Flux.update!(::Nothing, m::DoubleNetworkActorCriticModel, grads::Tuple)
     Flux.update!(m, grads)
 end
 
-function Flux.update!(m::DoubleNetworkActorCriticModel, grads::Tuple) 
-    Flux.update!(m, grads[1], grads[2])
+function Flux.update!(m::DoubleNetworkActorCriticModel, grads...) 
+    Flux.update!.(Ref(m.opt), Ref(m), grads)
 end
 
-function Flux.update!(m::DoubleNetworkActorCriticModel, pgrads, vgrads) 
-    Flux.update!(m.policymodelopt, m.policymodel, pgrads[1])
-    Flux.update!(m.valuemodelopt, m.valuemodel, vgrads[1])
-end
-
-function train!(m::DoubleNetworkActorCriticModel, states, actions, rewards, λ::Union{Nothing, Float32} = nothing, opt::Nothing = nothing; valuelossweight = 1.0, policylossweight = 1.0, entropylossweight = 1.0, γ = 1.0) 
+function Flux.train!(m::DoubleNetworkActorCriticModel, states, actions, rewards, λ::Union{Nothing, Float32} = nothing, opt::Nothing = nothing; valuelossweight = 1.0, policylossweight = 1.0, entropylossweight = 1.0, γ = 1.0) 
     flatrewards = reduce(vcat, rewards)
     flatactions = reduce(vcat, actions)
 
@@ -40,61 +38,3 @@ function train!(m::DoubleNetworkActorCriticModel, states, actions, rewards, λ::
 
     train!(m, states, flatactions, flatrewards, λ; valuelossweight, policylossweight, entropylossweight, γ)
 end
-
-function Flux.withgradient(m::DoubleNetworkActorCriticModel, states, actions, rewards::Vector{R}, λ::Union{Nothing, Float32} = nothing; valuelossweight = 1.0, policylossweight = 1.0, entropylossweight = 1.0, γ = 1.0) where R <: Real
-    T = length(rewards) 
-    discounts = γ.^range(0, T - 1)
-    returns = [sum(discounts[1:(T - t + 1)] .* rewards[t:end]) for t in 1:T] 
-
-    values = nothing 
-
-    vval, vgrads = Flux.withgradient(m.valuemodel, states, returns) do valuemodel, s, r
-        values = 𝒱(valuemodel, s) |> Flux.cpu |> vec 
-
-        return valuelossweight * ℒ(valuemodel, values, r[1:(end - 1)])
-    end
-
-    isfinite(vval) || @warn "Value loss is $vval"
-
-    if λ ≡ nothing
-        pop!(returns)
-
-        Ψ = returns - values
-    else
-        push!(values, last(rewards))
-
-        λ_discounts = (λ * γ).^range(0, T - 1) 
-
-        advs = rewards[1:(end - 1)] + γ * values[2:end] - values[1:(end - 1)]
-        Ψ = [sum(λ_discounts[1:(T - t)] .* advs[t:end]) for t in 1:(T - 1)] 
-    end
-
-    pop!(discounts)
-
-    pval, pgrads = Flux.withgradient(m.policymodel, states, actions, discounts, Ψ) do policymodel, s, a, d, Ψ 
-        pdist = π(policymodel, s) |> Flux.cpu 
-        ent_lpdf = reduce(hcat, [Distributions.entropy(coldist), log(coldist[colact])] for (coldist, colact) in zip(eachcol(pdist), a))
-
-        entropyloss = - mean(ent_lpdf[1, :])
-        policyloss = - mean(d .* Ψ .* ent_lpdf[2,:]) 
-
-        return policylossweight * policyloss + entropylossweight * entropyloss 
-    end
-
-    isfinite(pval) || @warn "Policy loss is $pval"
-
-    return Tuple(((pg,), (vg,)) for (pg, vg) in zip(pgrads, vgrads))
-end
-
-function train!(m::DoubleNetworkActorCriticModel, states, actions, rewards::Vector{R}, λ::Union{Nothing, Float32} = nothing; valuelossweight = 1.0, policylossweight = 1.0, entropylossweight = 1.0, γ = 1.0) where R <: Real
-    grads = Flux.withgradient(m, states, actions, rewards, λ; valuelossweight, policylossweight, entropylossweight, γ)  
-
-    try
-        Flux.update!(m, grads[1][1], grads[1][2])
-    catch e
-        throw(GradientException(m, states, actions, nothing, e, nothing, 1, length(rewards), (λ * γ).^range(0, length(rewards) - 1), (pgrads, vgrads)))
-    end
-
-    return grads 
-end
-
