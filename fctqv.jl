112f9a0e225058e95eff3c39d5e27bbec4b707d7
Karim Naguib <karimn2.0@gmail.com>
2023-06-27 19:19:14 -0400

    Easier access to the twin models

--- fctqv.jl
+++ fctqv.jl
@@ -9,7 +9,13 @@ function FCTQV(inputdims::Int, outputdims::Int, hiddendims::Vector{Int} = [32, 3
     return FCTQV(FCQSA(inputdims, outputdims, hiddendims; actfn, lossfn, usegpu), FCQSA(inputdims, outputdims, hiddendims; actfn, lossfn, usegpu))
 end
 
-ℒ(m::FCTQV, v̂₁, v̂₂, v) = ℒ(m.model₁, v̂₁, v) + ℒ(m.model₂, v̂₂, v)
 𝒬₁(m::FCTQV, state, action) = 𝒬(m.model₁, state, action)
 𝒬₂(m::FCTQV, state, action) = 𝒬(m.model₂, state, action)
 𝒬(m::FCTQV, state, action) = 𝒬₁(m, state, action), 𝒬₂(m, state, action) 
+min𝒬(m::FCTQV, state, action) = min.(vec.(Flux.cpu(𝒬(m, state, action)))...)
+ℒ₁(m::FCTQV, s, a, v) = ℒ(m.model₁, vec(𝒬₁(m, s, a)), v)
+ℒ₂(m::FCTQV, s, a, v) = ℒ(m.model₂, vec(𝒬₂(m, s, a)), v)
+ℒ₁(m::FCTQV, v̂, v) = ℒ(m.model₁, v̂, v)
+ℒ₂(m::FCTQV, v̂, v) = ℒ(m.model₂, v̂, v)
+ℒ(m::FCTQV, v̂₁, v̂₂, v) = ℒ₁(m, v̂₁, v) + ℒ₂(m, v̂₂, v)
+ℒ(m::FCTQV, s, a, v) = ℒ₁(m, s, a, v) + ℒ₂(m, s, a, v)
